ffe55aeef217e95e6a23fc8bc7ae97e13047a31b
Karthik Meher <kar.meher95@gmail.com>
2016-03-13 22:27:53 +0530

    Implemented gridview graphs and tabs

--- main_gui.py
+++ main_gui.py
@@ -1,7 +1,6 @@
 """
 
-Codebase taken from : https://github.com/eliben/code-for-blog/blob/master/2008/wx_mpl_dynamic_graph.py
-
+Initial working model taken from : https://github.com/eliben/code-for-blog/blob/master/2008/wx_mpl_dynamic_graph.py
 Author: Karthik Duddu
 
 """
@@ -109,32 +108,26 @@ class BoundControlBox(wx.Panel):
     def manual_value(self):
         return self.value
 
+class ControlPanel(wx.Panel):
 
-class GraphFrame(wx.Panel):
-    """ The main frame of the application
-    """
-    title = 'Demo: dynamic matplotlib graph'
-    
-    def __init__(self, top_panel):
-        # wx.Frame.__init__(self, None, -1, self.title)
-        super(GraphFrame, self).__init__(top_panel)
-        
-        self.datagen = DataGen()
-        self.data = [self.datagen.next()]
-        self.paused = False
-        
+    def __init__(self, top_panel):        
+        super(ControlPanel, self).__init__(top_panel)        
         self.create_main_panel()
-        
-        self.redraw_timer = wx.Timer(self)
-        self.Bind(wx.EVT_TIMER, self.on_redraw_timer, self.redraw_timer)        
-        self.redraw_timer.Start(100)
+
+    def on_pause_button(self, event):
+        self.paused = not self.paused
     
+    def on_update_pause_button(self, event):
+        label = "Resume" if self.paused else "Pause"
+        self.pause_button.SetLabel(label)
+    
+    def on_cb_grid(self, event):
+        self.draw_plot()
+    
+    def on_cb_xlab(self, event):
+        self.draw_plot()
+
     def create_main_panel(self):
-        # self.panel = wx.Panel(self)
-
-        self.init_plot()
-        self.canvas = FigCanvas(self, -1, self.fig)
-
         self.xmin_control = BoundControlBox(self, -1, "X min", 0)
         self.xmax_control = BoundControlBox(self, -1, "X max", 50)
         self.ymin_control = BoundControlBox(self, -1, "Y min", 0)
@@ -171,12 +164,51 @@ class GraphFrame(wx.Panel):
         self.hbox2.Add(self.ymax_control, border=5, flag=wx.ALL)
         
         self.vbox = wx.BoxSizer(wx.VERTICAL)
-        self.vbox.Add(self.canvas, 1, flag=wx.LEFT | wx.TOP | wx.GROW)        
         self.vbox.Add(self.hbox1, 0, flag=wx.ALIGN_LEFT | wx.TOP)
         self.vbox.Add(self.hbox2, 0, flag=wx.ALIGN_LEFT | wx.TOP)
         
         self.SetSizer(self.vbox)
-        self.vbox.Fit(self)    
+        self.vbox.Fit(self)
+
+class GraphFrame(wx.Panel):
+    """ The main frame of the application
+    """
+    title = 'Demo: dynamic matplotlib graph'
+    
+    def __init__(self, top_panel, data_stream):
+        # wx.Frame.__init__(self, None, -1, self.title)
+        super(GraphFrame, self).__init__(top_panel)
+        
+        self.datagen = data_stream
+        self.data = [self.datagen.next()]
+        self.paused = False
+        
+        self.create_main_panel()
+        
+        self.redraw_timer = wx.Timer(self)
+        self.Bind(wx.EVT_TIMER, self.on_redraw_timer, self.redraw_timer)        
+        self.redraw_timer.Start(1000)
+    
+    def create_main_panel(self):
+        # self.panel = wx.Panel(self)
+        self.init_plot()
+        self.canvas = FigCanvas(self, -1, self.fig)
+
+        # self.pause_button = wx.Button(self, -1, "Pause")
+        # self.Bind(wx.EVT_BUTTON, self.on_pause_button, self.pause_button)
+        # self.Bind(wx.EVT_UPDATE_UI, self.on_update_pause_button, self.pause_button)
+        
+        # self.hbox1 = wx.BoxSizer(wx.HORIZONTAL)
+        # self.hbox1.Add(self.pause_button, border=5, flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL)
+        # self.hbox1.AddSpacer(20)
+        
+        self.vbox = wx.BoxSizer(wx.VERTICAL)
+        self.vbox.Add(self.canvas, 1, flag=wx.LEFT | wx.TOP | wx.GROW)        
+        # self.vbox.Add(self.hbox1, 0, flag=wx.ALIGN_LEFT | wx.TOP)        
+        
+        self.SetSizer(self.vbox)
+        self.vbox.Fit(self)
+        
 
     def init_plot(self):
         self.dpi = 100
@@ -204,16 +236,16 @@ class GraphFrame(wx.Panel):
         # when xmin is on auto, it "follows" xmax to produce a 
         # sliding window effect. therefore, xmin is assigned after
         # xmax.
-        #
-        if self.xmax_control.is_auto():
-            xmax = len(self.data) if len(self.data) > 50 else 50
-        else:
-            xmax = int(self.xmax_control.manual_value())
+        
+        # if self.xmax_control.is_auto():
+        xmax = len(self.data) if len(self.data) > 50 else 50
+        # else:
+        #     xmax = int(self.xmax_control.manual_value())
             
-        if self.xmin_control.is_auto():            
-            xmin = xmax - 50
-        else:
-            xmin = int(self.xmin_control.manual_value())
+        # if self.xmin_control.is_auto():            
+        xmin = xmax - 50
+        # else:
+        #     xmin = int(self.xmin_control.manual_value())
 
         # for ymin and ymax, find the minimal and maximal values
         # in the data set and add a mininal margin.
@@ -221,16 +253,16 @@ class GraphFrame(wx.Panel):
         # note that it's easy to change this scheme to the 
         # minimal/maximal value in the current display, and not
         # the whole data set.
-        # 
-        if self.ymin_control.is_auto():
-            ymin = round(min(self.data), 0) - 1
-        else:
-            ymin = int(self.ymin_control.manual_value())
+
+        # if self.ymin_control.is_auto():
+        ymin = round(min(self.data), 0) - 1
+        # else:
+        #     ymin = int(self.ymin_control.manual_value())
         
-        if self.ymax_control.is_auto():
-            ymax = round(max(self.data), 0) + 1
-        else:
-            ymax = int(self.ymax_control.manual_value())
+        # if self.ymax_control.is_auto():
+        ymax = round(max(self.data), 0) + 1
+        # else:
+        #     ymax = int(self.ymax_control.manual_value())
 
         self.axes.set_xbound(lower=xmin, upper=xmax)
         self.axes.set_ybound(lower=ymin, upper=ymax)
@@ -240,35 +272,21 @@ class GraphFrame(wx.Panel):
         # so just passing the flag into the first statement won't
         # work.
         #
-        if self.cb_grid.IsChecked():
-            self.axes.grid(True, color='gray')
-        else:
-            self.axes.grid(False)
+        # if self.cb_grid.IsChecked():
+        self.axes.grid(True, color='gray')
+        # else:
+        #     self.axes.grid(False)
 
         # Using setp here is convenient, because get_xticklabels
         # returns a list over which one needs to explicitly 
         # iterate, and setp already handles this.
         #  
         pylab.setp(self.axes.get_xticklabels(), 
-            visible=self.cb_xlab.IsChecked())
+            visible=True) #self.cb_xlab.IsChecked()
         
         self.plot_data.set_xdata(np.arange(len(self.data)))
-        self.plot_data.set_ydata(np.array(self.data))
-        
+        self.plot_data.set_ydata(np.array(self.data))        
         self.canvas.draw()
-    
-    def on_pause_button(self, event):
-        self.paused = not self.paused
-    
-    def on_update_pause_button(self, event):
-        label = "Resume" if self.paused else "Pause"
-        self.pause_button.SetLabel(label)
-    
-    def on_cb_grid(self, event):
-        self.draw_plot()
-    
-    def on_cb_xlab(self, event):
-        self.draw_plot()
     
     def on_redraw_timer(self, event):
         # if paused do not add data, but still redraw the plot
@@ -281,6 +299,41 @@ class GraphFrame(wx.Panel):
     
     def on_exit(self, event):
         self.Destroy()
+
+class GraphTab(wx.Panel):
+    def __init__(self, top_panel):
+        # wx.Frame.__init__(self, None, -1, self.title)
+        super(GraphTab, self).__init__(top_panel)
+        ds = DataGen()
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        # control_panel = ControlPanel(self) 
+        gs = wx.GridSizer(3, 3, 10, 10)
+        graphs_list = []
+        for i in range(6):
+            graphs_list.append(GraphFrame(self, ds))
+            gs.Add(graphs_list[i], 0, wx.EXPAND)
+        # gs.Add(control_panel, 0, wx.EXPAND)
+
+        # fgs = wx.FlexGridSizer(2, 1, 9, 25)
+        # fgs.Add(p1,0,wx.EXPAND|wx.ALL,border=10)
+        # fgs.Add(p2,0,wx.EXPAND|wx.ALL,border=10)
+        # fgs.AddGrowableRow(2, 1)
+        # fgs.AddGrowableCol(1, 1)
+        # gs.AddMany( [
+        #         (wx.Button(self, label='*'), 0, wx.EXPAND),
+        #         (wx.Button(self, label='1'), 0, wx.EXPAND),
+        #         (wx.Button(self, label='2'), 0, wx.EXPAND),
+        #         (wx.Button(self, label='3'), 0, wx.EXPAND),
+        #         (wx.Button(self, label='-'), 0, wx.EXPAND),
+        #         (wx.Button(self, label='0'), 0, wx.EXPAND),
+        #         (wx.Button(self, label='.'), 0, wx.EXPAND),
+        #         (wx.Button(self, label='='), 0, wx.EXPAND),
+        #         (wx.Button(self, label='+'), 0, wx.EXPAND) 
+        #     ])       
+
+        hbox.Add(gs, proportion=1, flag=wx.ALL|wx.EXPAND, border=15)
+        self.SetSizer(hbox)
+
 
 class Example(wx.Frame):
 
@@ -297,19 +350,17 @@ class Example(wx.Frame):
     def InitUI(self):
     
         panel = wx.Panel(self)
-        hbox = wx.BoxSizer(wx.HORIZONTAL)
-        fgs = wx.FlexGridSizer(3, 2, 9, 25)
-        p1 = GraphFrame(panel)
-        p2 = GraphFrame(panel)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        w, h = self.GetClientSizeTuple()        
+        sub_panel = GraphFrame(panel, DataGen())
+        notebook = wx.Notebook(panel)
 
-        fgs.Add(p1,0,wx.EXPAND|wx.ALL,border=10)
-        fgs.Add(p2,0,wx.EXPAND|wx.ALL,border=10)
+        tabOne = GraphTab(notebook)
+        notebook.AddPage(tabOne, "Vitals Graphs")
 
-        fgs.AddGrowableRow(2, 1)
-        fgs.AddGrowableCol(1, 1)
-
-        hbox.Add(fgs, proportion=1, flag=wx.ALL|wx.EXPAND, border=15)
-        panel.SetSizer(hbox)
+        sizer.Add(sub_panel, 1, wx.EXPAND)
+        sizer.Add(notebook, 1, wx.EXPAND)
+        panel.SetSizer(sizer)
 
     def on_exit(self, event):
         self.Destroy()
@@ -360,7 +411,7 @@ class Example(wx.Frame):
 
 if __name__ == '__main__':
     app = wx.App(redirect=True, filename="log.txt")
-    Example(None, title="Review")
+    Example(None, title="ICU Alarm System")
     # app.frame = GraphFrame()
     # app.frame.Show()
     app.MainLoop()
